4902595b68511993155a3b5c972e6a85e5aa2688
Pallavi0119 <104244682+Pallavi0119@users.noreply.github.com>
2024-09-03 17:21:55 +0530

    Update AddDevice.dart

--- lib/AddDevice.dart
+++ lib/AddDevice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QRScannerPage extends StatefulWidget {
   @override
@@ -12,12 +13,14 @@ class _QRScannerPageState extends State<QRScannerPage>
   String? scannedQRCode;
   String message = "Position the QR code inside the scanner";
   late MobileScannerController _controller;
+  String? _email;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
     _controller = MobileScannerController();
+    _loadEmail();
   }
 
   @override
@@ -30,7 +33,9 @@ class _QRScannerPageState extends State<QRScannerPage>
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
-      _controller.start();
+      if (scannedQRCode == null) {
+        _controller.start();
+      }
     } else if (state == AppLifecycleState.paused) {
       _controller.stop();
     }
@@ -40,16 +45,78 @@ class _QRScannerPageState extends State<QRScannerPage>
     setState(() {
       scannedQRCode = null;
       message = "Position the QR code inside the scanner";
-      _controller.stop();
-      Future.delayed(Duration(milliseconds: 500), () {
-        _controller.start();
-      });
+      _controller.start(); // Resume the scanner
     });
   }
 
   Future<bool> _onWillPop() async {
     _controller.stop();
     return true;
+  }
+
+  Future<void> _loadEmail() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? email = prefs.getString('email');
+
+    if (email != null) {
+      setState(() {
+        _email = email;
+      });
+    } else {
+      // Handle case where email is not found, e.g., navigate to sign-in page
+      Navigator.pushReplacementNamed(context, '/signin');
+    }
+  }
+
+  Future<void> _showConfirmationDialog(String scannedQRCode) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Device Addition'),
+          content: Text('Do you want to add this device'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                _addDevice(scannedQRCode); // Call the API
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _addDevice(String deviceID) async {
+    final String apiUrl =
+        "https://ymfmk699j5.execute-api.us-east-1.amazonaws.com/default/Cloudsense_user_add_devices?email_id=$_email&device_id=$deviceID";
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          message = "Device ID updated successfully.";
+        });
+      } else {
+        setState(() {
+          message = "Failed to add device. Please try again.";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        message = "An error occurred: $e";
+      });
+    }
   }
 
   @override
@@ -77,11 +144,14 @@ class _QRScannerPageState extends State<QRScannerPage>
                   allowDuplicates: false,
                   onDetect: (barcode, args) {
                     final String? code = barcode.rawValue;
-                    if (code != null) {
+                    if (code != null && code != scannedQRCode) {
+                      // If a new QR code is scanned, process it
                       setState(() {
                         scannedQRCode = code;
-                        message = "Detected QR Code: $scannedQRCode";
+                        message = "Detected QR Code";
                       });
+                      _controller.stop(); // Stop the scanner
+                      _showConfirmationDialog(code); // Show confirmation dialog
                     } else {
                       setState(() {
                         message = "No valid QR Code detected";
