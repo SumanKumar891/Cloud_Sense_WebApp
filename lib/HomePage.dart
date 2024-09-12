3c8d47179489d584c37fca49c57b083a492daf65
Pallavi0119 <pallavikrishnan01@gmail.com>
2024-09-12 16:46:27 +0530

    testedWithDifferentImage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -138,7 +138,7 @@ class _HomePageState extends State<HomePage> {
               Stack(
                 children: [
                   Image.asset(
-                    'assets/weatherr.jpg', // Replace with your image path
+                    'assets/soil.jpg', // Replace with your image path
                     width: double.infinity,
                     height: MediaQuery.of(context).size.width < 800 ? 400 : 550,
                     fit: BoxFit.cover,
