1eb7e896ff2b99ceb1d4a04879682ff59480907d
Sejal-08 <sejalsankhyan2001@gmail.com>
2024-09-12 17:46:57 +0530

    Zooming Behaviour

--- lib/DeviceGraphPage.dart
+++ lib/DeviceGraphPage.dart
@@ -518,6 +518,7 @@ class _DeviceGraphPageState extends State<DeviceGraphPage> {
         ? Padding(
             padding: const EdgeInsets.all(16.0),
             child: Container(
+              width: double.infinity,
               height: 400,
               margin: EdgeInsets.all(10),
               decoration: BoxDecoration(
@@ -545,35 +546,38 @@ class _DeviceGraphPageState extends State<DeviceGraphPage> {
                     ),
                   ),
                   Expanded(
-                    child: SfCartesianChart(
-                      plotAreaBackgroundColor: Colors.white,
-                      primaryXAxis: DateTimeAxis(
-                        dateFormat: DateFormat('hh:mm a'),
-                        title: AxisTitle(
-                          text: 'Time',
-                          textStyle: TextStyle(fontWeight: FontWeight.bold),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: SfCartesianChart(
+                        plotAreaBackgroundColor: Colors.white,
+                        primaryXAxis: DateTimeAxis(
+                          dateFormat: DateFormat('hh:mm a'),
+                          title: AxisTitle(
+                            text: 'Time',
+                            textStyle: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          labelRotation: 70,
+                          edgeLabelPlacement: EdgeLabelPlacement.shift,
                         ),
-                        labelRotation: 70,
-                        edgeLabelPlacement: EdgeLabelPlacement.shift,
-                      ),
-                      primaryYAxis: NumericAxis(
-                        title: AxisTitle(
-                          text: yAxisTitle,
-                          textStyle: TextStyle(fontWeight: FontWeight.bold),
+                        primaryYAxis: NumericAxis(
+                          title: AxisTitle(
+                            text: yAxisTitle,
+                            textStyle: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          axisLine: AxisLine(width: 1),
+                          majorGridLines: MajorGridLines(width: 1),
                         ),
-                        axisLine: AxisLine(width: 1),
-                        majorGridLines: MajorGridLines(width: 1),
+                        tooltipBehavior: TooltipBehavior(enable: true),
+                        // zoomPanBehavior: ZoomPanBehavior(
+                        //   zoomMode: ZoomMode.x,
+                        //   enablePanning: true,
+                        //   enablePinching: true,
+                        // enableMouseWheelZooming: true,
+                        // ),
+                        series: <ChartSeries<ChartData, DateTime>>[
+                          _getChartSeries(chartType, data, title),
+                        ],
                       ),
-                      tooltipBehavior: TooltipBehavior(enable: true),
-                      zoomPanBehavior: ZoomPanBehavior(
-                        zoomMode: ZoomMode.x,
-                        enablePanning: true,
-                        enablePinching: true,
-                        enableMouseWheelZooming: true,
-                      ),
-                      series: <ChartSeries<ChartData, DateTime>>[
-                        _getChartSeries(chartType, data, title),
-                      ],
                     ),
                   ),
                   // Add zoom control buttons
