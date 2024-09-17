4217b121c892aa4bf9d9720394ae44561f87ca57
Sejal-08 <sejalsankhyan2001@gmail.com>
2024-09-17 12:03:05 +0530

    Image Testing

--- lib/DeviceGraphPage.dart
+++ lib/DeviceGraphPage.dart
@@ -383,7 +383,7 @@ class _DeviceGraphPageState extends State<DeviceGraphPage> {
     // Determine the background image based on the device type
     String backgroundImagePath = widget.deviceName.startsWith('WD')
         ? 'assets/tree.jpg'
-        : 'assets/Chloritron.PNG';
+        : 'assets/Chloritronn.png';
     // : 'assets/soil.jpg';
 
     return Scaffold(
