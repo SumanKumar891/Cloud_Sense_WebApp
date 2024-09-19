1a21687e7d76993bac64b8a563c6aa9b3c0f9a36
Pallavi0119 <pallavikrishnan01@gmail.com>
2024-09-19 16:28:55 +0530

    updated build file

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -90,14 +90,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "7290e403e99f13c7f92a4c8263de34e9
 "flutter_assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "flutter_assets/packages/wakelock_plus/assets/no_sleep.js": "9c3aa3cd0b217305aa860decab3d9f42",
 "flutter_assets/shaders/ink_sparkle.frag": "9bb2aaa0f9a9213b623947fa682efa76",
-"flutter_bootstrap.js": "2bb3a0762e2af3888c036560d30db1bc",
+"flutter_bootstrap.js": "f3886e2791a9e766637aff3c19e47b07",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ac3d76aa9e36fe5b3bb2600f7c12437d",
 "/": "ac3d76aa9e36fe5b3bb2600f7c12437d",
-"main.dart.js": "59b8762adcf63ac50612d93e22ba2899",
+"main.dart.js": "ae9f32d22ee5a2e4ca10ea4abb916fd5",
 "manifest.json": "1945d941ea0a66a4fc873921c259c902",
 "version.json": "c99f477bf3954abaf222bbd37a2f300d"};
 // The application shell files that are downloaded before a service worker can
